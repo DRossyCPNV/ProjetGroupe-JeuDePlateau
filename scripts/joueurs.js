bd4d8fb41aba7ce754ddedb392bd4c8646ad9e69
Luca Coduri <luca.coduri@cpnv.ch>
2019-05-18 18:42:06 +0200

    Ajout de commentaire à joueurs.js

--- scripts/joueurs.js
+++ scripts/joueurs.js
@@ -1,7 +1,7 @@
 //les couleurs de pions disponibles
 const couleursPions = ["rouge", "bleuClair", "bleuFonce", "orange", "violet", "vert"];
 
-//Function pour crée un tableau objet joueur
+//Function pour crée un tableau objet joueur, n étant le nombre de joueur
 function maker(n) {
     var arr = [];
     for(i = 0 ; i<n;i++) {
@@ -11,7 +11,7 @@ function maker(n) {
     }
     return arr;
 }
-
+//L'objet joueur, contient un id, la couleur de pion, une sommme d'argent de départ, une section
 function Joueur(id, nom, couleur, argent_depart, section){
     var that = this;
     this.id = id;
@@ -25,13 +25,16 @@ function Joueur(id, nom, couleur, argent_depart, section){
     var imgPion = new Image();
     imgPion.src = "images/pions/" + couleur + ".png";
 
+    //fonction pour placer le pion du joueur sur la case départ
     this.placerPionCaseDepart = function () {
+        //quand l'image a chargée
         imgPion.onload = function () {
+            //si on est au 3 eme pion
             if(id > 2){
-                ctx.drawImage(imgPion, ((that.id - 3)*(15 * echelle)) + coordCaseDep.X, (25 * echelle) + coordCaseDep.Y, 15 * echelle, 25 * echelle);// (id) car l'id commence à 0. Cela permettra de décaler le pion de chaque joueurs.
-                console.log("un petit modulo est passé");
+                // (id - 3) car l'id commence à 0. Cela permettra de décaler le pion de chaque joueurs.
+                ctx.drawImage(imgPion, ((that.id - 3)*(15 * echelle)) + coordCaseDep.X, (25 * echelle) + coordCaseDep.Y, 15 * echelle, 25 * echelle);
             }else{
-                ctx.drawImage(imgPion, ((that.id)*(15 * echelle)) + coordCaseDep.X, coordCaseDep.Y, 15 * echelle, 25 * echelle);// (id) car l'id commence à 0. Cela permettra de décaler le pion de chaque joueurs.
+                ctx.drawImage(imgPion, ((that.id)*(15 * echelle)) + coordCaseDep.X, coordCaseDep.Y, 15 * echelle, 25 * echelle);
             }
         }
 
