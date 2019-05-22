62b8e512ad18a1433589141934276c342929a4c6
BARRAUD Laurent <Laurent.Barraud@cpnv.ch>
2019-05-22 11:51:53 +0200

    animation du dé et affichage de la dernière valeur, retenue pour le déplacement

--- scripts/jeu.js
+++ scripts/jeu.js
@@ -1,5 +1,11 @@
-//Ce programme va devenir un programme de dessin
-//avec des boutons pour dessiner differentes choses
+// **************************************************************
+// Modulopoly - Gameloop
+// JCY, Laurent Barraud, Bastian Chollet, Guillaume Duvoisin, 
+// Guilain Mbayo, David Rossy & Luca Coduri - SI-CA1a - mai 2019 
+// **************************************************************
+
+
+// Ce programme dessine differentes choses au moyen de boutons
 
 var c = document.getElementById("plateau_jeu");
 var ctx = c.getContext("2d");
@@ -8,17 +14,22 @@ var ctx = c.getContext("2d");
 var img = new Image();
 img.src = "images/six-faces-de.jpg";
 
-//parametre de dessin
+// parametre de dessin
 var echelle = 1.7; //pour pouvoir zoomer
 var angle = 0;// pour pouvoir tourner
 var decx = 400;
-var decy = 400; //origine au milieu du plateau de jeu
-var tcx = 50;//taille carte en x
+var decy = 400; // origine au milieu du plateau de jeu
+var tcx = 50;// taille carte en x
 var tcy = 100;
 const ncartes = 5;//nombre de carte par cote (sans compter les coins)
 var acartes = [];
 
-//Donn�es
+// parametres du dé
+var tabNombres = new Array(); // tableau qui contient les nombres générés aléatoirement
+var nbFacesAffichees = 0; // compte le nombre de fois qu'une face de dé est affichée
+var resultatDe = 0; // stocke la dernière valeur affichée par le dé, utilisée pour le déplacement des pions.
+
+//Donnees
 $.getJSON('donnees/cases.json', function(data) {
     acartes = data;
 });
@@ -55,7 +66,6 @@ function fnCarte(n) {
     ctx.font = 5 * echelle + "pt Arial";
     fnText(acartes[n].texte, (ncartes / 2 + 0.5 - acartes[n].ordre) * tcx, (ncartes / 2) * tcx + 0.5 * tcy + 10, "dimgray");
 
-    //
     ctx.rotate(-angle);
 
 }
@@ -78,7 +88,18 @@ function fnText(t, x, y, c) {
     //Dessine un texte intelligent, dans une couleur donnee tenant compte de l'echelle
     ctx.fillStyle = c;
     ctx.fillText(t, echelle * x, echelle * y);
+	
 }
+
+function fnDraw(img, p1, p2, p3, p4, p5, p6, p7, p8)
+{
+	//Dessine une image intelligente, à l'échelle.
+	
+			//Dessine l'image déclarée au début du fichier, avec (1) la source, (2,3) les coordonnées x et y du coin haut-gauche, (4,5) la largeur et hauteur, 
+		// (6,7) les coordonnées x et y du coin haut-gauche où dessiner l'image sur le canvas, (8,9) la largeur et hauteur voulue.
+	ctx.drawImage(img, p1, p2, p3, p4, p5 * echelle, p6 * echelle, p7 * echelle, p8 * echelle);	
+}
+
 
 function fnJeu() {
     //Cette fonction dessine le plateau de jeu entier
@@ -92,70 +113,56 @@ function fnJeu() {
         fnCarte(i);
 
     }
-    ctx.translate(-decx, -decy); //on place l'origine en decx,
+    ctx.translate(-decx, -decy); //on remet au coin supérieur-gauche l'origine
 
 }
 
 function fnLancerDe() {
-	//Cette fonction va afficher neuf faces du dé au hasard et s'arrêter sur la dixième.
-	
+	//Cette fonction va tirer 6 nombres au hasard
+		
 	//Crée un tableau pour stocker les nombres
-	var tabNombres = new Array();
+	var nbAffiche = 0;
 	
 	//Stocke des entiers aléatoires entre une valeur min (incluse)
 	//et une valeur max (incluse) dans le tableau
-	var min = 1;
-	var max = 60;
-	tabNombres[0] = Math.floor(Math.random() * (max - min +1)) + min;
-	tabNombres[1] = Math.floor(Math.random() * (max - min +1)) + min;
-	tabNombres[2] = Math.floor(Math.random() * (max - min +1)) + min;
-	tabNombres[3] = Math.floor(Math.random() * (max - min +1)) + min;
-	tabNombres[4] = Math.floor(Math.random() * (max - min +1)) + min;
-	tabNombres[5] = Math.floor(Math.random() * (max - min +1)) + min;
-	var nbChoisi = 0;
+	var min = 0;
+	var max = 59;
 	
-		for(var i=0; i<tabNombres.length; i++) {
-			
-			randomNumber = tabNombres[i];
-		
-				if (randomNumber <= 10) {
-					//Dessine l'image déclarée au début du fichier, avec (1) la source, (2,3) les coordonnées x et y du coin haut-gauche, (4,5) la largeur et hauteur, 
-					// (6,7) les coordonnées x et y du coin haut-gauche où dessiner l'image sur le canvas, (8,9) la largeur et hauteur voulue.
-					// dessine la face "6"
-					ctx.drawImage(img, 15, 15, 141, 140, 935, 40, 101, 100);	
-					nbChoisi = 6;
-			
-				} else if (randomNumber > 10 && randomNumber <= 20) {
-					// dessine la face "5"
-					ctx.drawImage(img, 172, 15, 141, 140, 935, 40, 101, 100);	
-					nbChoisi = 5;
-				
-				} else if (randomNumber > 20 && randomNumber <= 30) {
-					// dessine la face "4"
-					ctx.drawImage(img, 329, 15, 141, 140, 935, 40, 101, 100);	
-					nbChoisi = 4;
-				
-				} else if (randomNumber > 30 && randomNumber <= 40) {
-					// dessine la face "3"
-					ctx.drawImage(img, 484, 15, 141, 140, 935, 40, 101, 100);	
-					nbChoisi = 3;
-			
-				} else if (randomNumber > 40 && randomNumber <= 50) {
-					// dessine la face "2"
-					ctx.drawImage(img, 640, 15, 141, 140, 935, 40, 101, 100);	
-					nbChoisi = 2;
-				
-				
-				} else if (randomNumber > 50) {		
-					// dessine la face "1"
-					ctx.drawImage(img, 796, 15, 141, 140, 935, 40, 101, 100);	
-					nbChoisi = 1;
-				}
-					
-		
-		}
+	for(var i=0; i<6; i++) {
+		tabNombres[i] = Math.floor(Math.random() * (max - min +1)) + min;		  
+	}
+	
+	// timer qui appelle la fonction toutes les 120 milli-secondes, pour l'animation du dé.
+	tmrAffiche = setInterval(fnAfficheFaceDe, 120);	
   
-  
-	return nbChoisi;
+	
+	
+	return nbAffiche;
 }
+
+function fnAfficheFaceDe() {
+	//Cette fonction va afficher une des 6 faces du dé, selon les nombres du tableau choisis au hasard	
+
+	randomNumber = tabNombres[nbFacesAffichees];
+	var faceDe= Math.floor(randomNumber/10); //génère un nombre aléatoire entre 0 et 5
+	
+			//Appelle la fonction qui dessine la face du dé intelligemment, à l'échelle.
+			fnDraw(img, 796-(156.2*faceDe), 15, 141, 140, 550, 23, 58, 58);	
+	
+	nbFacesAffichees++;
+	
+		if (nbFacesAffichees >= 6) 
+		{
+			ctx.font = 6 * echelle + "pt Arial";
+			resultatDe = faceDe+1;
+			fnText("Vous avez fait "+resultatDe+".", 580, 100, "chocolate");	
+			
+			//Remise à zéro du compteur
+			nbFacesAffichees = 0;
+			clearInterval(tmrAffiche);
+		}
+	
+}
+
+
 		
