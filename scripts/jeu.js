1110ac31d6a8174f2d80057fdbc38083fbdbfe86
BARRAUD Laurent <Laurent.Barraud@cpnv.ch>
2019-05-22 09:10:00 +0200

    de affichant un nombre au hasard

--- scripts/jeu.js
+++ scripts/jeu.js
@@ -1,20 +1,21 @@
 //Ce programme va devenir un programme de dessin
-//avec des boutons pour dessiner diff�rentes choses
+//avec des boutons pour dessiner differentes choses
 
 var c = document.getElementById("plateau_jeu");
 var ctx = c.getContext("2d");
 
 // Le reste du script ici....
 var img = new Image();
+img.src = "images/six-faces-de.jpg";
 
-//param�tre de dessin
+//parametre de dessin
 var echelle = 1.7; //pour pouvoir zoomer
 var angle = 0;// pour pouvoir tourner
 var decx = 400;
 var decy = 400; //origine au milieu du plateau de jeu
 var tcx = 50;//taille carte en x
 var tcy = 100;
-const ncartes = 5;//nombre de carte par cot� (sans compter les coins)
+const ncartes = 5;//nombre de carte par cote (sans compter les coins)
 var acartes = [];
 
 //Donn�es
@@ -28,7 +29,6 @@ function fnEfface() {
     //Effacement du canvas
 
     ctx.clearRect(0, 0, c.width, c.height);
-
 }
 
 function fnCarte(n) {
@@ -37,7 +37,7 @@ function fnCarte(n) {
     //dessin du rectangle autour
     ctx.lineWidth = 2 * echelle;//2 pixels de largeur de trait
 
-    //Si on n'est pas dans la rang�e 1, il faut tourner
+    //Si on n'est pas dans la rangee 1, il faut tourner
     var angle = (acartes[n].cote - 1) * Math.PI / 2;
     ctx.rotate(angle);
 
@@ -62,7 +62,7 @@ function fnCarte(n) {
 
 function fnRect(x, y, lx, ly, c1, c2) {
     //Cette fonction dessine un rectangle
-    //Dessine un texte intelligent, dans une couleur donn�e tenant compte du cot� et de l'�chelle
+    //Dessine un texte intelligent, dans une couleur donn�e tenant compte du cote et de l'echelle
 
     if (c1 != 0) {
         ctx.fillStyle = c1;//couleur du trait
@@ -75,16 +75,16 @@ function fnRect(x, y, lx, ly, c1, c2) {
 }
 
 function fnText(t, x, y, c) {
-    //Dessine un texte intelligent, dans une couleur donn�e tenant compte de l'�chelle
+    //Dessine un texte intelligent, dans une couleur donnee tenant compte de l'echelle
     ctx.fillStyle = c;
     ctx.fillText(t, echelle * x, echelle * y);
 }
 
 function fnJeu() {
-    //Cette fonction dessind le plateau de jeu entier
+    //Cette fonction dessine le plateau de jeu entier
     ctx.translate(decx, decy); //on place l'origine en decx, decy
 
-    //Dessin du carr� (plateau jaune)
+    //Dessin du carre (plateau jaune)
     fnRect(-ncartes / 2 * tcx - tcy, -ncartes / 2 * tcx - tcy, 2 * tcy + ncartes * tcx, 2 * tcy + ncartes * tcx, "yellow", "black");
 
     //Dessin de toutes les cartes
@@ -95,4 +95,67 @@ function fnJeu() {
     ctx.translate(-decx, -decy); //on place l'origine en decx,
 
 }
+
+function fnLancerDe() {
+	//Cette fonction va afficher neuf faces du dé au hasard et s'arrêter sur la dixième.
 	
+	//Crée un tableau pour stocker les nombres
+	var tabNombres = new Array();
+	
+	//Stocke des entiers aléatoires entre une valeur min (incluse)
+	//et une valeur max (incluse) dans le tableau
+	var min = 1;
+	var max = 60;
+	tabNombres[0] = Math.floor(Math.random() * (max - min +1)) + min;
+	tabNombres[1] = Math.floor(Math.random() * (max - min +1)) + min;
+	tabNombres[2] = Math.floor(Math.random() * (max - min +1)) + min;
+	tabNombres[3] = Math.floor(Math.random() * (max - min +1)) + min;
+	tabNombres[4] = Math.floor(Math.random() * (max - min +1)) + min;
+	tabNombres[5] = Math.floor(Math.random() * (max - min +1)) + min;
+	var nbChoisi = 0;
+	
+		for(var i=0; i<tabNombres.length; i++) {
+			
+			randomNumber = tabNombres[i];
+		
+				if (randomNumber <= 10) {
+					//Dessine l'image déclarée au début du fichier, avec (1) la source, (2,3) les coordonnées x et y du coin haut-gauche, (4,5) la largeur et hauteur, 
+					// (6,7) les coordonnées x et y du coin haut-gauche où dessiner l'image sur le canvas, (8,9) la largeur et hauteur voulue.
+					// dessine la face "6"
+					ctx.drawImage(img, 15, 15, 141, 140, 935, 40, 101, 100);	
+					nbChoisi = 6;
+			
+				} else if (randomNumber > 10 && randomNumber <= 20) {
+					// dessine la face "5"
+					ctx.drawImage(img, 172, 15, 141, 140, 935, 40, 101, 100);	
+					nbChoisi = 5;
+				
+				} else if (randomNumber > 20 && randomNumber <= 30) {
+					// dessine la face "4"
+					ctx.drawImage(img, 329, 15, 141, 140, 935, 40, 101, 100);	
+					nbChoisi = 4;
+				
+				} else if (randomNumber > 30 && randomNumber <= 40) {
+					// dessine la face "3"
+					ctx.drawImage(img, 484, 15, 141, 140, 935, 40, 101, 100);	
+					nbChoisi = 3;
+			
+				} else if (randomNumber > 40 && randomNumber <= 50) {
+					// dessine la face "2"
+					ctx.drawImage(img, 640, 15, 141, 140, 935, 40, 101, 100);	
+					nbChoisi = 2;
+				
+				
+				} else if (randomNumber > 50) {		
+					// dessine la face "1"
+					ctx.drawImage(img, 796, 15, 141, 140, 935, 40, 101, 100);	
+					nbChoisi = 1;
+				}
+					
+		
+		}
+  
+  
+	return nbChoisi;
+}
+		
