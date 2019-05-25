ca20c0c360749d13c4f274f3d4f821e8e6573a34
Luca Coduri <luca.coduri@cpnv.ch>
2019-05-25 21:56:47 +0200

    Ajout du popup configuration

---
+++ scripts/menu.js
@@ -0,0 +1,6 @@
+$("#btn-start").click(function () {
+    $("#cfg-bg").css("visibility","visible").css("opacity","1");
+});
+$("#croix").click(function () {
+    $("#cfg-bg").css("visibility","hidden").css("opacity","0");
+});
