60e735b75d395432c73b9f479fb7d8042544b46b
Luca Coduri <luca.coduri@cpnv.ch>
2019-05-29 19:11:49 +0200

    Ajout du fichier gameloop.js + explication du futur contenu du fichier

---
+++ scripts/gameloop.js
@@ -0,0 +1,13 @@
+/*todo
+*  - Attendre que le dé soit lancé
+*  - Actualiser l'affichage des éléments sur le plateau
+*  - Regarder l'effet qu'a la case sur la quel le joueur se trouve actuellement
+*  - appliquer l'effet
+*  - joueur suivant
+*  - Recommencer
+* */
+
+//cette fonction sera la fonction principale qui liera toutes les autres pour rendre le jeu fonctionnel
+function gameloop(){
+
+}
